24e3c1aebea1ac03d869be90669680e9804658f7
kai [she] <coilysiren@gmail.com>
2026-07-17 22:35:32 -0700

    fix: unblock native Windows commits (closes #1481, closes #1482)

--- cmd/ward/disk_other.go
+++ cmd/ward/disk_other.go
@@ -4,7 +4,7 @@ package main
 
 import "fmt"
 
-var surfaceScratchDiskFreeBytes = func(path string) (free, total uint64, err error) {
+var surfaceScratchDiskFreeBytes = func(_ string) (free, total uint64, err error) {
 	return 0, 0, fmt.Errorf("disk usage unavailable on this platform")
 }
 
